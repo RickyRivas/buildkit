a417672833091bf02e9969f38fde52cd787cbf4b
Ricky Rivas <rickyrivas918@gmail.com>
2023-07-26 13:56:10 -0500

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+  import "$styles/global.less"
+  import Navigation from "$lib/components/Navigation.svelte"
+</script>
+
+<Navigation />
+
+<main>
+  <slot />
+</main>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+  import "$styles/home.less"
+</script>
+
+<section id="landing">
+  <div class="content">
+    <span>Made with Sveltekit</span>
+    <h1>Web development,<br /> streamlined</h1>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptates adipisci et magni
+      laborum, voluptatibus omnis soluta numquam neque eaque inventore.
+    </p>
+    <a href="/" class="cta">Read the docs</a>
+  </div>
+  <img
+    class="hero"
+    src="/img01.jpg"
+    alt="laptop"
+    width="500"
+    height="610"
+    decoding="async"
+    fetchpriority="low" />
+</section>
