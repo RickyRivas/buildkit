a8856337c99b6b9f5fca8f3b5b81f3290412fd28
Ricky Rivas <rickyrivas918@gmail.com>
2023-07-26 17:08:46 -0500

    Home page meta tags and structure

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,27 @@
 <script>
+  // components
+
+  // stores
+  import { URL, companyName, industry, cityAndState, pages, services } from "$lib/config"
+  import { page } from "$app/stores"
+  // styles
   import "$styles/home.less"
+
+  // logic
+  const thisPage = pages.find((p) => p.path === $page.route.id)
 </script>
+
+<svelte:head>
+  <!-- preload hero imgs -->
+  <!-- <link rel="preload" as="image" href="/heromobile.webp" />
+  <link rel="preload" as="image" href="/hero.webp" /> -->
+  <meta property="og:title" content="" />
+  <link rel="canonical" href={URL} />
+  <meta property="og:url" content={URL} />
+  <meta name="description" content={thisPage?.metaDescription} />
+  <meta property="og:description" content={thisPage?.metaDescription} />
+  <title>{companyName} | {industry} | {cityAndState}</title>
+</svelte:head>
 
 <section id="landing">
   <div class="content">
@@ -12,12 +33,5 @@
     </p>
     <a href="/" class="cta">Read the docs</a>
   </div>
-  <img
-    class="hero"
-    src="/img01.jpg"
-    alt="laptop"
-    width="500"
-    height="610"
-    decoding="async"
-    fetchpriority="low" />
+  <img class="hero" src="/img01.jpg" alt="laptop" width="500" height="610" decoding="async" />
 </section>
