213e0e98da5e1b85e8d342195a0d3f5bbcc0a4cc
Ricky Rivas <rickyrivas918@gmail.com>
2023-07-26 17:28:27 -0500

    interior pages default

---
+++ src/lib/components/SubNavigation.svelte
@@ -0,0 +1,59 @@
+<script lang="ts">
+  // components
+  // stores
+  // styles
+  // logic
+  // props
+  export let pageTitle: any
+</script>
+
+<section id="subheader">
+  <div class="container">
+    <h1>{pageTitle}</h1>
+  </div>
+</section>
+
+<style lang="less">
+  #subheader {
+    background: #000;
+    display: flex;
+    justify-content: center;
+    align-items: flex-end;
+    padding: 8em 0 2em;
+    position: relative;
+    isolation: isolate;
+    @media only screen and(min-width: 768px) {
+      text-align: left;
+      padding: 0 0 3em 0;
+      height: (373/20em);
+    }
+    &::before {
+      content: "";
+      width: 100%;
+      height: 100%;
+      position: absolute;
+      top: 0;
+      left: 0;
+      z-index: -1;
+      background: linear-gradient(90.01deg, #27631a 16.86%, rgba(0, 0, 0, 0) 100%);
+    }
+
+    .container {
+      text-align: center;
+      width: 96%;
+      max-width: (1280/20em);
+      margin: 0 auto;
+      @media only screen and(min-width: 768px) {
+        text-align: left;
+      }
+    }
+
+    h1 {
+      color: white;
+      font-weight: bold;
+      font-size: (49/20em);
+      line-height: 1.2;
+      text-transform: uppercase;
+    }
+  }
+</style>
--- src/routes/services/+page.svelte
+++ src/routes/services/+page.svelte
@@ -1,2 +1,24 @@
+<script lang="ts">
+  // components
+  import SubNavigation from "$lib/components/SubNavigation.svelte"
 
+  // stores
+  import { URL, companyName, industry, cityAndState, pages } from "$lib/config"
+  import { page } from "$app/stores"
+  // styles
+
+  // logic
+  const thisPage = pages.find((p) => p.path === $page.route.id)
+  console.log(thisPage)
+  const pageTitle = $page.route.id?.slice(1)
+</script>
+
+<svelte:head>
+  <link rel="canonical" href={URL + $page.route.id} />
+  <meta name="description" content={thisPage?.metaDescription} />
+  <meta property="og:description" content={thisPage?.metaDescription} />
+  <title>{companyName} | {thisPage?.name} | {cityAndState}</title>
+</svelte:head>
+
+<SubNavigation {pageTitle} />
 <h1>services</h1>
