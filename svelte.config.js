4cf6a74528c1f97d42c5c2414f3f9c618a8574de
Ricky Rivas <rickyrivas918@gmail.com>
2024-04-30 01:38:43 -0500

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import adapter from '@sveltejs/adapter-netlify';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { mdsvex } from 'mdsvex';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
