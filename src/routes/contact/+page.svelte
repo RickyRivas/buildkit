84aea910fa99fd847ae19be35bcfc6c71a6fcce0
Ricky Rivas <rickyrivas918@gmail.com>
2024-10-06 16:01:09 -0500

    custom loader and file upload for contact form

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -6,6 +6,7 @@
   import InteriorLayout from "$lib/components/InteriorLayout.svelte"
   import AnchorButton from "$lib/components/AnchorButton.svelte"
   import Modal from "$lib/components/Modal.svelte"
+  import LoadingStatus from "$lib/components/LoadingStatus.svelte"
 
   // stores
   import { business } from "$lib/config"
@@ -17,12 +18,12 @@
   let thisPage = findPage(business, $page.route.id)
 
   let form
-  let showModal = true
+  let showModal = false
   let loading = false
   let success = false
   let error = false
   let message = ""
-  let inProd = true
+  let inProd = false
 
   function clearModal() {
     if (success) clearFields()
@@ -32,6 +33,52 @@
     loading = false
     error = false
   }
+
+  function checkFileSize(input) {
+    document.querySelector(`label[for=${input.name}]`)?.classList.remove("success")
+    document.querySelector(`label[for=${input.name}]`)?.classList.remove("error")
+    const maxSize = 4 * 1024 * 1024 // 8MB in bytes
+    const files = input.element.files
+    let totalSize = 0
+
+    for (let i = 0; i < files.length; i++) {
+      totalSize += files[i].size
+    }
+
+    if (totalSize > maxSize) {
+      document.querySelector(`label[for=${input.name}]`).classList.add("error")
+      document.querySelector(`label[for=${input.name}] .custom-file-upload`).textContent =
+        "Maximum file size limit of 4mb reached. Try again."
+      return
+    }
+
+    let combinedFileNames = ""
+
+    for (let i = 0; i < files.length; i++) {
+      totalSize += files[i].size
+      combinedFileNames += files[i].name
+      if (i < files.length - 1) {
+        combinedFileNames += ", "
+      }
+    }
+
+    // // manually update file name
+    document.querySelector(`label[for=${input.name}]`)?.classList.add("success")
+    document.querySelector(`label[for=${input.name}] .custom-file-upload`).textContent =
+      combinedFileNames
+  }
+
+  let fileUploadInputs = [
+    {
+      id: "files",
+      name: "files",
+      label: "Supporting Files",
+      type: "file",
+      filename: "Please upload your supporting file (4mb max)",
+      element: "",
+      success: false,
+    },
+  ]
 </script>
 
 <!-- recaptha -->
@@ -72,15 +119,6 @@
   name="" />
 <SubNavigation pageTitle={thisPage?.title} pagePath={thisPage.path} pageName={thisPage.name} />
 
-{#if showModal}
-  <Modal
-    on:escape={() => {
-      showModal = false
-    }}>
-    <p>Our reservation booking software is temporarily unavailable. Please check back soon.</p>
-  </Modal>
-{/if}
-
 <InteriorLayout bind:thisPage>
   <h2>Get in touch.</h2>
   <p>
@@ -114,9 +152,8 @@
         success = true
         setTimeout(() => {
           goto("/")
-        }, 5000)
+        }, 3000)
       }
-      console.log(response)
       cancel()
     }}>
     <input type="hidden" name="form-name" value="contact" />
@@ -155,19 +192,71 @@
         rows="5"
         placeholder="Enter message..." />
     </div>
+
     <!-- Uncomment for file uploads -->
-    <!-- <div class="form-control">
-        <label for="design-upload">Upload your design:</label>
-        <input
-          name="file"
-          type="file"
-          bind:this={fileUploadInput}
-          on:change={checkFileSize}
-          accept="image/*" />
-      </div> -->
+    <div class="form-controls-uploads">
+      {#each fileUploadInputs as input}
+        <!-- 1st file upload -->
+        <div class="form-control">
+          <label for={input.name} class={input.success ? "success" : ""}>
+            <div class="icon">
+              <svg
+                aria-hidden="true"
+                role="img"
+                xmlns="http://www.w3.org/2000/svg"
+                width="76"
+                height="71"
+                fill="none"
+                viewBox="0 0 76 71">
+                <path fill="#1A1918" d="M.149 62.707h75v7.759h-75v-7.759Z" />
+                <path
+                  fill="#1A1918"
+                  d="M33.77 54.954V14.83L15.748 31.905l-5.338-5.632L37.649.473l27.237 25.8-5.337 5.632-18.021-17.074v40.123h-7.759Z" />
+              </svg>
+            </div>
+            <span class="custom-file-upload">
+              {input.filename}
+            </span>
+          </label>
+          <input
+            class="screenreader"
+            name={input.name}
+            type="file"
+            id={input.id}
+            bind:this={input.element}
+            on:change={() => {
+              checkFileSize(input)
+            }}
+            required
+            accept="image/*,.pdf,.doc,.docx,.xls,.xlsx" />
+        </div>
+      {/each}
+    </div>
     <!-- recaptcha -->
     <!-- <div class="g-recaptcha" style="margin-bottom: 1em;" id="g-recaptcha" data-sitekey="" /> -->
     <AnchorButton text="Submit" formButton={true} disabled={true} />
     <!-- -->
   </form>
 </InteriorLayout>
+
+{#if showModal}
+  <Modal
+    disable={loading ? true : false}
+    on:escape={() => {
+      clearModal()
+    }}>
+    <div class="subscribe-page-modal">
+      <LoadingStatus bind:loading bind:success bind:error />
+      {#if loading}
+        <p>Loading...</p>
+      {/if}
+      {#if success}
+        <!-- successfully added to db -->
+        <p>{message}</p>
+      {:else if error}
+        <!-- failed to be added to db -->
+        <p>{message}</p>
+      {/if}
+    </div>
+  </Modal>
+{/if}
